dcfdf0c47a937dfa4134e2b83d109a7e1d2f70d8
thehanifz <thehanifz@gmail.com>
2026-01-22 09:55:06 +0700

    Delete frontend directory

--- frontend/src/components/table/renderCell.tsx
+++
@@ -1,9 +0,0 @@
-import { useTaskStore } from "../../state/taskStore";
-import { StatusCell } from "./StatusCell";
-
-export function renderCell(row: any, col: string) {
-  if (col === "StatusPekerjaan" || col === "Detail Progres") {
-    return <StatusCell row={row} col={col} />;
-  }
-  return <span className="truncate">{row.data?.[col] ?? "-"}</span>;
-}
